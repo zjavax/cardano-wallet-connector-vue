cfa0b401afeee4a0377e75b19717064339cf5c30
zjavax <1306187161@qq.com>
2024-03-28 13:45:26 +0800

    服务器配置太低  分析包排除包

--- vite.config.ts
+++ vite.config.ts
@@ -16,10 +16,15 @@ import {
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss'
+import * as dotenv from 'dotenv'
+
+// 打包体积分析 npm i rollup-plugin-visualizer -D
+import { visualizer } from 'rollup-plugin-visualizer'
+
+// 打包 CDN 引入 external 排除
+import externalGlobals from 'rollup-plugin-external-globals'
 
 const pathSrc = path.resolve(__dirname, 'src')
-
-import * as dotenv from 'dotenv'
 
 dotenv.config()
 
@@ -75,7 +80,60 @@ export default defineConfig({
       target: 'esnext',
     },
   },
+
   build: {
+    assetsDir: 'assets',
+    chunkSizeWarningLimit: 1000,
+    // 配置打包文件路径和命名
+    minify: 'terser',
+    outDir: 'tool-plus',
+    // 取消计算文件大小，加快打包速度
+    reportCompressedSize: false,
+    sourcemap: false,
     target: 'esnext',
+    terserOptions: {
+      compress: {
+        // 生产环境时移除console.log调试代码
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      // // 打包时忽略某些包，避免打包时间过长
+      external: [
+        'vue',
+        'vue-router',
+        'pinia',
+        'vue-demi',
+        'ant-design-vue',
+        'antd',
+        'icons-vue',
+        'icons-svg',
+        'colors',
+        'dayjs',
+        'prismjs',
+        'sql-formatter',
+        // 以下3个包是在使用ant-design-vue的时候始终排除不掉的，感觉ChatGPT协助我解决了这个问题，原来可以使用正则表达式排除
+        /^@ant-design\/icons-vue/,
+        /^@ant-design\/icons-svg/,
+        /^@ant-design\/colors/,
+      ],
+      plugins: [
+        // // 打包时忽略某些包，避免打包时间过长
+        externalGlobals({
+          vue: 'Vue',
+          'vue-router': 'VueRouter',
+          pinia: 'Pinia',
+          'vue-demi': 'VueDemi',
+          'ant-design-vue': 'antd',
+          dayjs: 'dayjs',
+          prismjs: 'Prism',
+          axios: 'axios',
+          // qrcode: 'QRCode',
+          clipboard: 'ClipboardJS',
+          'sql-formatter': 'sqlFormatter',
+        }) as any,
+      ],
+    },
   },
 })
